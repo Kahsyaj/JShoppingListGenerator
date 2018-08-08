178b0c8d6533d3d129a5d2891ced4bca96620d9a
Jérémy Coley <jcoley@digitaleo.com>
2018-08-08 17:25:52 +0200

    Started eventListeners

--- app/src/main/java/personnal/ahsyaj/jshoppinglistgenerator/CategoryActivity.java
+++ app/src/main/java/personnal/ahsyaj/jshoppinglistgenerator/CategoryActivity.java
@@ -2,9 +2,11 @@ package personnal.ahsyaj.jshoppinglistgenerator;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
-public class CategoryActivity extends AppCompatActivity {
+public class CategoryActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button backButton = null;
     private Button addButton = null;
@@ -13,10 +15,30 @@ public class CategoryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.category_layout);
+        this.setContentView(R.layout.category_layout);
 
-        this.backButton = findViewById(R.id.backButton);
+        this.backButton = findViewById(R.id.genBackButton);
         this.addButton = findViewById(R.id.addButton);
+
+        this.backButton.setOnClickListener(this);
+        this.addButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Button clicked = (Button) view;
+        if (view.getId() == this.addButton.getId()) {
+            TextView category = this.findViewById(R.id.subTitle);
+
+            this.setContentView(R.layout.item_layout);
+
+            TextView itemName = this.findViewById(R.id.itemName);
+
+            itemName.setHint(String.format("%s name", category.getText().subSequence(0, (category.getText().length() - 1 ))));
+
+        } else {
+            this.finish();
+        }
     }
 }
 
--- app/src/main/java/personnal/ahsyaj/jshoppinglistgenerator/MainActivity.java
+++ app/src/main/java/personnal/ahsyaj/jshoppinglistgenerator/MainActivity.java
@@ -1,9 +1,12 @@
 package personnal.ahsyaj.jshoppinglistgenerator;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private Button ingredientsButton = null;
@@ -13,17 +16,45 @@ public class MainActivity extends AppCompatActivity {
     private ImageButton quitButton = null;
     private ImageButton setupButton = null;
 
+    private class CustomButtonOnClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            switch (view.getId()) {
+                case R.id.quitButton:
+                    MainActivity.this.finish();
+                case R.id.setupButton:
+                    MainActivity.this.setContentView(R.layout.setup_layout);
+                default:
+                    MainActivity.this.setContentView(R.layout.category_layout);
+
+                    Button clicked = (Button)view;
+                    TextView subTitle = MainActivity.this.findViewById(R.id.subTitle);
+
+                    subTitle.setText(clicked.getText());
+            }
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_main);
+        this.setContentView(R.layout.activity_main);
 
         this.ingredientsButton = findViewById(R.id.ingredientsButton);
         this.mealsButton = findViewById(R.id.mealsButton);
         this.shoppingListsButton = findViewById(R.id.shoppinglistsbutton);
         this.generateButton = findViewById(R.id.generateButton);
         this.quitButton = findViewById(R.id.quitButton);
-        this.setupButton = findViewById(R.id.addButton);
+        this.setupButton = findViewById(R.id.setupButton);
+
+        this.ingredientsButton.setOnClickListener(new CustomButtonOnClickListener());
+        this.mealsButton.setOnClickListener(new CustomButtonOnClickListener());
+        this.shoppingListsButton.setOnClickListener(new CustomButtonOnClickListener());
+        this.generateButton.setOnClickListener(new CustomButtonOnClickListener());
+        this.quitButton.setOnClickListener(new CustomButtonOnClickListener());
+        this.setupButton.setOnClickListener(new CustomButtonOnClickListener());
     }
+
+
 }
